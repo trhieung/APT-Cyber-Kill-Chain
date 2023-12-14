372c58fd08d7cbb60ddaf8bce8ab550bfbc25078
trhieung <nthieu.ngtr@gmail.com>
2023-12-14 21:26:20 +0700

    s

--- test_gophish.sh
+++ test_gophish.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Check if gophish module is installed
+if ! python3 -c "import gophish" 2>/dev/null; then
+    echo "gophish module not found. Installing..."
+    pip install gophish
+fi
+
+# Run your Python script
 python3 tools/gophish/custom_gophish.py
