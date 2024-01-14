7816adc33376176d5c2bce82484b0b70fd72421d
trhieung <nthieu.ngtr@gmail.com>
2024-01-14 14:20:51 +0700

    s

--- tools/apache2/custom_apache2.sh
+++ tools/apache2/custom_apache2.sh
@@ -2,3 +2,4 @@
 sudo chown www-data:www-data ./var/www/apt_domain/*.exe
 sudo chown www-data:www-data ./var/www/apt_domain/sessions/*.exe
 sudo chown www-data:www-data ./var/www/apt_domain/beacons/*.exe
+sudo chown www-data:www-data ./var/www/apt_domain/configs/*.cfg
