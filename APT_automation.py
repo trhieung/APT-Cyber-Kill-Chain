2b436e6c7f3c68d286556719433a11c509318a8f
trhieung <nthieu.ngtr@gmail.com>
2024-02-16 09:10:19 +0700

    setting

--- APT_automation.py
+++ APT_automation.py
@@ -20,7 +20,7 @@ class reconaissance:
 
         load_dotenv()
         print(self)
-        # self.start_spiderfoot()
+        self.start_spiderfoot()
 
     def __str__(self) -> str:
         return (
@@ -33,28 +33,42 @@ class reconaissance:
             "- Username\n"
         )
 
-    def run_shell_script_with_env(script_path):
+    def run_shell_script_with_env(self, script_path):
         # Load environment variables from .env file
         load_dotenv()
 
         # Collect parameters from environment variables
         domain = os.getenv("WEB_DOMAIN")
         output_file = os.getenv("EMAIL_FILE")
-        output_directory = os.getenv("PATH_EMAILS")
+        temp = os.path.join(".", os.getenv("PATH_EMAILS"))
+        output_directory = temp[:-1]
+        file_path = os.path.join(output_directory, output_file)
+
+        # if file exist -> delete
+        if os.path.exists(file_path): 
+            os.remove(file_path)
 
         try:
             # Run the shell script with the collected parameters
             subprocess.run(["bash", script_path, "-d", domain, "-o", output_file, "-r", output_directory], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             print("Shell script executed successfully.")
+
+            # Show taken emails
+            if (os.path.exists(file_path)):
+                with open(file_path, 'r') as f:
+                    content = f.read()
+                    print(content)
+            else: print(f"file in path {file_path} not found!")
+
         except subprocess.CalledProcessError as e:
             print(f"Error executing shell script: {e}")
 
     def start_spiderfoot(self):
         # Provide the path to your shell script
-        script_path = "test_spiderfoot.sh"
+        script_path = "./tools/spiderfoot/emails_from_domain.sh"
 
         # Call the function to run the shell script
-        run_shell_script(script_path)
+        self.run_shell_script_with_env(script_path)
 
 def main():
     list_phase = []
