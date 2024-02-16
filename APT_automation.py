258df58350b2efa9dc48170df0fae1dc56aba546
trhieung <nthieu.ngtr@gmail.com>
2024-02-16 08:42:10 +0700

    setting

--- APT_automation.py
+++ APT_automation.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 
 def run_shell_script(script_path):
     try:
-        # Run the shell script
-        subprocess.run(["bash", script_path], check=True)
+        # # Run the shell script
+        # subprocess.run(["bash", script_path], check=True)
+        # Run the shell script silently
+        subprocess.run(["bash", script_path], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         print("Shell script executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing shell script: {e}")
@@ -13,10 +15,12 @@ def run_shell_script(script_path):
 class reconaissance:
     def __init__(self) -> None:
         self.web_domain = ""
+        self.output_file = ""
+        self.output_directory = ""
 
         load_dotenv()
-        self.get_web_domain()
-        self.start_spiderfoot()
+        print(self)
+        # self.start_spiderfoot()
 
     def __str__(self) -> str:
         return (
@@ -28,9 +32,22 @@ class reconaissance:
             "- Email gateway\n"
             "- Username\n"
         )
-    
-    def get_web_domain(self):
-        self.web_domain = os.getenv("WEB_DOMAIN")
+
+    def run_shell_script_with_env(script_path):
+        # Load environment variables from .env file
+        load_dotenv()
+
+        # Collect parameters from environment variables
+        domain = os.getenv("WEB_DOMAIN")
+        output_file = os.getenv("EMAIL_FILE")
+        output_directory = os.getenv("PATH_EMAILS")
+
+        try:
+            # Run the shell script with the collected parameters
+            subprocess.run(["bash", script_path, "-d", domain, "-o", output_file, "-r", output_directory], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            print("Shell script executed successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing shell script: {e}")
 
     def start_spiderfoot(self):
         # Provide the path to your shell script
@@ -42,8 +59,8 @@ class reconaissance:
 def main():
     list_phase = []
     list_phase.append(reconaissance())
-    a = list_phase[0]
-    print(a)
+    # a = list_phase[0]
+    # print(a)
 
 if __name__ == '__main__':
     main()
